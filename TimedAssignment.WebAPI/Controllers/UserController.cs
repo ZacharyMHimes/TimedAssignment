4eaebbefc2681481ababc1d6a532cc1f735babf9
Zachary Himes <zacharymhimes@gmail.com>
2022-12-21 23:19:36 -0500

    added Post and User Controllers

---
+++ TimedAssignment.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimedAssignment.Services.Post;
+using TimedAssignment.Services.User;
+
+namespace TimedAssignment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IPostService _postService;
+        
+    }
+}
